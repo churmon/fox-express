b5912470bb16d5dd82116c4ca170e76bd5bdcc9b
churmon <churmonmalanda@gmail.com>
2026-07-13 07:15:52 +0200

    refactor: comment out server listen and logging for cleaner export

--- src/server.js
+++ src/server.js
@@ -61,6 +61,8 @@ app.use((err, req, res, next) => {
   return res.status(500).json({ message: err.message || "Something went wrong" });
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running on http://localhost:${PORT}`);
+// });
+
+export default app;
